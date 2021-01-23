ac0b7bf4fdb215ff6a987a2c9f0f9eaf03fff355
Marulina-Stewart <ferchichimarwa@ymail.com>
2021-01-23 18:55:16 +0100

    Dom

---
+++ shopping_cart/js/script.js
@@ -0,0 +1,88 @@
+let heart_buttons = document.querySelectorAll(".heart");
+let delete_buttons = document.querySelectorAll(".delete-btn");
+let plus_btn = document.querySelectorAll(".plus-btn");
+let min_btn = document.querySelectorAll(".minus-btn");
+let product_price = document.querySelectorAll(".product-price");
+let total_price = document.querySelector(".total-price .final-price");
+let shop_cart = document.querySelector(".shopping-cart");
+let confirm_button = document.getElementById("confirm_command");
+
+let totalprice_product = 0;
+
+function total() {
+  product_price.forEach(function (item) {
+    totalprice_product += Number(item.lastChild.textContent);
+  });
+}
+
+window.onload = function () {
+  total();
+  total_price.textContent = totalprice_product;
+};
+
+heart_buttons.forEach(function (item) {
+  item.onclick = function () {
+    if (item.classList.length === 1) {
+      item.classList.add("heart-clicked");
+    } else {
+      item.classList.remove("heart-clicked");
+    }
+  };
+});
+
+delete_buttons.forEach(function (item) {
+  item.onclick = function () {
+    let price = item.parentElement.parentElement.lastElementChild.lastChild;
+    item.parentElement.parentElement.remove();
+    totalprice_product -= Number(price.textContent);
+    total_price.textContent = totalprice_product;
+    price.textContent = 0;
+    if (shop_cart.children.length == 1) {
+      confirm_button.style.display = "none";
+      shop_cart.innerHTML = "<h1>there is no product in the cart</h1>";
+    }
+  };
+});
+
+function old_price(item) {
+  return Number(item.parentElement.nextElementSibling.lastChild.textContent);
+}
+function official_price(item) {
+  return Number(item.parentElement.nextElementSibling.getAttribute("price"));
+}
+
+plus_btn.forEach(function (item) {
+  item.onclick = function () {
+    totalprice_product = 0;
+    let oldprice = old_price(item);
+    let price = official_price(item);
+    let value = Number(item.previousElementSibling.getAttribute("value")) + 1;
+    item.previousElementSibling.setAttribute("value", value);
+    let newprice = oldprice + price;
+    item.parentElement.nextElementSibling.lastChild.textContent = newprice;
+    total();
+    total_price.textContent = totalprice_product;
+  };
+});
+
+min_btn.forEach(function (item) {
+  item.onclick = function () {
+    totalprice_product = 0;
+    let oldprice = old_price(item);
+    let price = official_price(item);
+    let value = Number(item.nextElementSibling.getAttribute("value"));
+    if (value > 1) {
+      value -= 1;
+      item.nextElementSibling.setAttribute("value", value);
+      let newprice = oldprice - price;
+      item.parentElement.nextElementSibling.lastChild.textContent = newprice;
+      total();
+      total_price.textContent = totalprice_product;
+    }
+  };
+});
+
+function confirm_command() {
+  alert("Suceess");
+}
+confirm_button.addEventListener("click", confirm_command);
